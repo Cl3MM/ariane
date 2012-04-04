a64458954516196c9f124c9d538fd690d9cdfae4
Clément Roullet <clement.roullet@gmail.com>
2012-04-04 18:06:58 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ariane::Application.config.secret_token = '36577a64d6ac4f3ed778de7e522bad5176a0ebf908ad57471503ac3c5818b359455956e51f72cf5721325b7071cac70266337ac36d911ce0f6a137e3a75c59d7'
