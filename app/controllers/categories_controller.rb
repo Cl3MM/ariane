44b4de4596e097a6eb820f244cfc9c17645d63f5
Clément Roullet <clement.roullet@gmail.com>
2012-04-10 02:14:25 +0200

    Added Portfolio front page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,8 @@ class CategoriesController < ApplicationController
   # GET /categories
   # GET /categories.json
   def index
-    @categories = Category.all
+    @category = Category.all
+    @models = Model.order("id DESC").paginate(:page => params[:page], :per_page => 10)
 
     respond_to do |format|
       format.html # index.html.erb
