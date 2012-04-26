e48ed9b03a32e78da0619f398c5e02a60dda1cdd
Clément Roullet <clement.roullet@gmail.com>
2012-04-26 18:03:28 +0200

    Trying to fix asset pipeline through Precompile Array (changed /assets/themes/ to themes)...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,8 +47,8 @@ Ariane::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   ## config.assets.precompile += %w( search.js /assets/galleria-1.2.7.min.js /assets/themes/classic/galleria.classic.min.js /assets/galleria.js /assets/themes/classic/galleria.classic.js )
-  config.assets.precompile += ["search.js", "galleria-1.2.7.min.js", "galleria.classic.min.js",
-                               "galleria.js", "galleria.classic.js", "/themes/classic/galleria.classic.css",
+  config.assets.precompile += ["search.js", "galleria-1.2.7.min.js", "themes/classic/galleria.classic.min.js",
+                               "galleria.js", "themes/classic/galleria.classic.js", "themes/classic/galleria.classic.css",
                                "generic.js" ]
 
 
